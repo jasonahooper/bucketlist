c1b40571f84e0c6623d8581fa4cdb6baa2344d41
Dan Garland <dan@dangarland.co.uk>
2013-11-07 15:24:59 +0000

    Adding blank rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BucketList::Application.config.secret_key_base = 'd8c722239720202272133f82c56a7b47e5fa52fd91b2099855c770f3c2b67791575694d794f6fd140abf1c9aa4172e4f4bac0bdb3efad832ec8836e9716b6127'
