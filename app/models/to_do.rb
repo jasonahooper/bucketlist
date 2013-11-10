e3dfe1ed66c3de4334c6b2d90934c3aca62920f9
Jason Hooper <jason.hooper@talk21.com>
2013-11-10 11:37:26 +0000

    geocoding working

--- app/models/to_do.rb
+++ app/models/to_do.rb
@@ -1,3 +1,11 @@
 class ToDo < ActiveRecord::Base
   belongs_to :destination
+  geocoded_by :geocode_full
+
+  after_validation :geocode
+
+  private
+    def geocode_full
+      "#{where} #{destination.name}"
+    end
 end
--- features/step_definitions/add_to_do_steps.rb
+++ features/step_definitions/add_to_do_steps.rb
@@ -8,8 +8,8 @@ When(/^I add a To\-Do$/) do
   # fill_in "ToDo Image URL", :with => 'image.jpg'
   fill_in "ToDo Priority", :with => rand(99)
   fill_in "ToDo Where", :with => "A Dummy Place"
-  fill_in "ToDo Latitude", :with => Faker::Address.latitude
-  fill_in "ToDo Longitude", :with => Faker::Address.longitude
+  # fill_in "ToDo Latitude", :with => Faker::Address.latitude
+  # fill_in "ToDo Longitude", :with => Faker::Address.longitude
   click_button 'Add ToDo'
 end
 
